35d5888c28d85f7fd705a267e85484d8a297b56c
sternsternstern <emil@kern.tk>
2025-09-18 20:13:36 +0200

    set up for audio work.. coming soon

--- funnyfaces/scenes/Audiomanager.cs
+++ funnyfaces/scenes/Audiomanager.cs
@@ -3,4 +3,35 @@ using System;
 
 public partial class Audiomanager : Control
 {
+    public static Audiomanager Instance { get; private set; }
+    public override void _Ready()
+    {
+        Instance = this;
+    }
+
+    public void StartMusic()
+    {
+
+    }
+    public void SwitchToCreditsMusic()
+    {
+
+    }
+
+    public void PlaySound_Powder()
+    {
+
+    }
+    public void PlaySound_NosePlace()
+    {
+
+    }
+    public void PlaySound_PaperRustle()
+    {
+
+    }
+    public void PlaySound_NoseFall()
+    {
+
+    }
 }
