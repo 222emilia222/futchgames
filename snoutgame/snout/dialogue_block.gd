c06fe5ce5edb327062b74e9a890a83c969c1c512
sternsternstern <74257676+222emilia222@users.noreply.github.com>
2026-08-17 18:05:50 +0200

    snout dialogue manager

---
+++ snoutgame/snout/dialogue_block.gd
@@ -0,0 +1,8 @@
+extends HBoxContainer
+
+@onready var character_label = %CharacterLabel
+@onready var dialogue_label = %DialogueLabel
+@onready var progress = %Progress
+
+func set_inactive() -> void:
+	
