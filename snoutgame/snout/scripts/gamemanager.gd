321495d4cb5c5fa95f1fb1d5258a6711c901cb7e
sternsternstern <74257676+222emilia222@users.noreply.github.com>
2026-08-20 19:49:16 +0200

    visuals
    POI animation + behavior; path sign; cleared flag

--- snoutgame/snout/scripts/gamemanager.gd
+++ snoutgame/snout/scripts/gamemanager.gd
@@ -4,11 +4,13 @@ var current_scene: Node3D
 @export var game_fov: float
 @export var camera_adj_speed: float
 @export var places: Dictionary[String, String]
+var last_interacted: Node3D
 
 func _ready() -> void:
 	var start_scene = load(places["start"])
 	var instance = start_scene.instantiate()
 	add_child(instance)
+	current_scene = instance
 	%Camera.fov = game_fov
 	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
 
@@ -20,9 +22,12 @@ func load_scene(key: String) -> void:
 		current_scene.queue_free()
 		#vllt process frame hier
 		var scene = load(places[key])
-		add_child(scene)
-		current_scene = scene
+		var instance = scene.instantiate()
+		add_child(instance)
+		current_scene = instance
+	else:
+		print("scene key (" + key + ") not found in places array")
 
 func _on_dialogue_ended(_resource: DialogueResource) -> void:
-	print("on dialogue ended signal got through")
 	get_child(0).lerp_fov(game_fov)
+	last_interacted.after_interaction()
--- snoutgame/snout/scripts/interactible.gd
+++ snoutgame/snout/scripts/interactible.gd
@@ -11,6 +11,7 @@ var player: Node3D
 var camera: Camera3D
 var target_rotation_player: Vector3
 var target_rotation_cam: Vector3
+var is_cleared = false
 
 func _ready() -> void:
 	root_node = get_tree().root.get_child(1)
@@ -21,14 +22,27 @@ func _ready() -> void:
 
 func interact() -> void:
 	print("interacted with " + name)
+	root_node.last_interacted = self
 	if is_path:
 		#load new environment
 		root_node.load_scene(scene_name)
 	else:
 		#trigger corresponding dialogue, implement cue 
 		DialogueManager.show_dialogue_balloon(dialogue, "start")
+		#trigger poi close animation
+		%Animator.play("close")
+		%Animator.hide()
+		%CollisionShape3D.disabled = true
+		#camera adjustment
 		player.lerp_fov(interact_fov)
 		var tween := create_tween()
 		tween.set_parallel()
 		tween.tween_property(camera.get_parent(), "rotation_degrees", target_rotation_player, root_node.camera_adj_speed)
 		tween.tween_property(camera, "rotation_degrees", target_rotation_cam, root_node.camera_adj_speed)
+
+func after_interaction() -> void:
+	if !is_cleared:
+		#trigger poi open animation
+		%Animator.play("open")
+		%Animator.show()
+		%CollisionShape3D.disabled = false
